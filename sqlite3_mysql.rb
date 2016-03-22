e24bc2565c56a62a7517503a23e1397fa98a4d14
David Visscher <david@davidvisscher.nl>
2016-03-22 15:07:10 +0100

    Added incompatible line removal

--- sqlite3_mysql.rb
+++ sqlite3_mysql.rb
@@ -1,4 +1,15 @@
 
+#Take it line by line
 ARGF.each do |line|
+  # Remove lines not included in MySQL
+  if line.include? "PRAGMA" \
+  or line.include? "COMMIT" \
+  or line.include? "BEGIN TRANSACTION" \
+  or line.include? "CREATE UNIQUE INDEX" \
+  or line.include? "sqlite_sequence"
+    line = ""
+  end
+  
+  #Write line to standard out
   $stdout.write line
 end
