920ba350ce120688e80c6b2ea071cd3b2c870643
David Visscher <david@davidvisscher.nl>
2016-03-22 15:28:20 +0100

    Added keyword replacement

--- sqlite3_mysql.rb
+++ sqlite3_mysql.rb
@@ -9,7 +9,17 @@ ARGF.each do |line|
   or line.include? "sqlite_sequence"
     line = ""
   end
-  
+
+  # Words we always want to replace
+  line = line.gsub('autoincrement','auto_increment')
+  line = line.gsub('AUTOINCREMENT','AUTO_INCREMENT')
+
+  line = line.gsub("DEFAULT 't'", "DEFAULT '1'")
+  line = line.gsub("DEFAULT 'f'", "DEFAULT '0'")
+
+  line = line.gsub(", 't'",", '1'")
+  line = line.gsub(", 'f'",", '0'")
+
   #Write line to standard out
   $stdout.write line
 end
